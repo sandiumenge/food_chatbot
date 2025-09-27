19956456a6f324023bf9ca9aa6954fc599979eb0
sandiumenge <jacob.sandiumenge@gmail.com>
2025-09-27 15:22:11 +0200

    Fix: Conversation model url basename changed from -user -> conversation

--- chat/api/urls.py
+++ chat/api/urls.py
@@ -2,8 +2,8 @@ from rest_framework.routers import DefaultRouter
 from chat.api.views import ConversationViewSet, ConversationMessageViewSet, UserProfileViewSet
 
 router = DefaultRouter()
-router.register('users', ConversationViewSet, basename='user')
+router.register('conversations', ConversationViewSet, basename='conversation')
 router.register('messages', ConversationMessageViewSet, basename='message')
-router.register('profiles', UserProfileViewSet, basename='profile')
+router.register('users', UserProfileViewSet, basename='user')
 
 urlpatterns = router.urls
